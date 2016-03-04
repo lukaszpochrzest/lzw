2839315adf1d94ea3ddc5c7d3708c4568c806ce3
Lukasz <luke.pochrzest@gmail.com>
2016-03-04 16:54:33 +0100

    Dodanie prostych testow jednostkowych

---
+++ src/test/java/org/lzw/LZWTest.java
@@ -0,0 +1,61 @@
+package org.lzw;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by lukasz on 04.03.16.
+ */
+public class LZWTest {
+
+    public static final String WIKI_DATA_EXAMPLE = "abccd_abccd_acd_acd_acd_";
+//    public static final String WIKI_DATA_EXAMPLE_ENCODED = "1, 2, 3, 3, 4, 5, 6, 8, 10, 1, 9, 11, 16, 15, 10"
+
+    private static final Logger LOGGER = Logger.getAnonymousLogger();
+
+    @Test
+    public void byteArrayIntTest() {
+
+        /** prepare test data   **/
+        int[] intArr = new int[] {0, 1, 255, 256, 312, 1033};
+        List<Integer> intList = new ArrayList<>(intArr.length);
+        for(int ind : intArr) {
+            intList.add(ind);
+        }
+
+        /** encode indexes  **/
+        EncodedData encodedData = LZW.indexesToByteArray(intList);
+
+        /** decode indexes  **/
+        int[] decodedInts = LZW.intsFromByteArray(encodedData.indexesByteArray, encodedData.bitsPerInt);
+
+        Assert.assertTrue(Arrays.equals(intArr, decodedInts));
+
+    }
+
+    @Test
+    public void encodeDecodeTest() {
+
+        /** prepare test data   **/
+        String dataString = WIKI_DATA_EXAMPLE;
+        byte[] data = dataString.getBytes(StandardCharsets.UTF_8);
+
+        /** encoding **/
+        EncodedData encodedData = LZW.encode(data);
+
+        /** decoding **/
+        byte[] decodedData = LZW.decode(encodedData);
+
+        String resultString = new String(decodedData, StandardCharsets.UTF_8);
+
+        Assert.assertTrue(dataString.equals(resultString));
+
+    }
+
+}
